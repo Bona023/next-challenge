df7af37a462db996742fdc9bd1e5ccc84ded9727
Bona_Kim <bona2324@gmail.com>
2024-11-07 22:55:48 +0900

    Forms & Actions Challenge

--- app/components/function.tsx
+++ app/components/function.tsx
@@ -1,4 +1,14 @@
 "use server";
-export default async function handleForm(formData: FormData) {
-    console.log(formData.get("email"), formData.get("username"), formData.get("password"));
+export default async function handleForm(prevState: any, formData: FormData) {
+    const password = formData.get("password");
+    if (password == "12345") {
+        return {
+            success: true,
+        };
+    } else {
+        return {
+            success: false,
+            errors: ["Wrong password"],
+        };
+    }
 }
